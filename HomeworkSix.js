0339c44d476a5a23c303692d73b16b792777d086
Cory Lozon <corylozon@Corys-MacBook-Air.local>
2017-06-26 21:52:38 -0400

    final version

--- HomeworkSix.js
+++ HomeworkSix.js
@@ -1,25 +1,27 @@
 var animalSearches = ["Rhino", "Lion", "Bear", "Kitten"];
 
-
-    function displayAnimalInfo() {
-      var animal = $(this).attr("data-name");
+    function displayAnimalInfo(animal) {
       var queryURL = "http://api.giphy.com/v1/gifs/search?q=" +
         animal + "&api_key=dc6zaTOxFJmzC&limit=10";
-    
+        console.log(queryURL + " animal: " + animal);
 
       $.ajax({
         url: queryURL,
         method: "GET"
       }).done(function(response) {
         var results = response.data
-        alert(results);
-        for (var i = 0; i < results.length; i++) {          // for loop to display results to search query
+        
+        for (var i = 0; i < results.length; i++) { 
+          console.log(results[i]);         // for loop to display results to search query
           var animalDiv = $("<div>");
           var p = $("<p>").text("Rating: " + results[i].rating);  // creates a p to hold the ratings
 
           var animalImage = $("<img>");                             // creats an image to hold the images with fixed height
           animalImage.attr("src", results[i].images.fixed_height.url);
-
+          animalImage.attr("data-animate", results[i].images.fixed_height.url);
+          animalImage.attr("data-state", "animate");
+          animalImage.attr("data-still", results[i].images.fixed_height_still.url);
+          animalImage.addClass("cat");
           animalDiv.prepend(p);
           animalDiv.prepend(animalImage);
 
@@ -30,7 +32,6 @@ var animalSearches = ["Rhino", "Lion", "Bear", "Kitten"];
     } // closes function displayanimal info
 
     function renderButtons() {
-        alert("in renderbuttons");
         $("#new-buttons").empty();                                  // empty for new button
         for (var j = 0; j < animalSearches.length; j++) {             // Looping the array of animals
           var a = $("<button>");
@@ -43,22 +44,23 @@ var animalSearches = ["Rhino", "Lion", "Bear", "Kitten"];
      
       $("#add-animal").on("click", function(event) {            // when the button is clicked, a function takes input from the text
         event.preventDefault();
-        alert("Final Function");
         var animal = $("#new-input").val().trim();
         animalSearches.push(animal);                          //adds animal to array           
         renderButtons();                               // Calling renderButtons which handles the processing of our animal array
       });             
 
       
-      $(document).on("click", ".animalSearches", displayAnimalInfo);      // Adding a click 
+      $(document).on("click", ".animal", function(){
+        var animal = $(this).attr('data-name');
+         displayAnimalInfo(animal);
+      });      // Adding a click 
 
                                                                     // Calling the renderButtons function to display the intial buttons
       renderButtons();
-      displayAnimalInfo();
-
-
-        $(".gif").on("click", function() {                    // function that stops and starts gifs
+      
+        $(document).on("click", ".cat", function() {                    // function that stops and starts gifs
       var state = $(this).attr("data-state");
+     
       if (state === "still") {
         $(this).attr("src", $(this).attr("data-animate"));
         $(this).attr("data-state", "animate");
